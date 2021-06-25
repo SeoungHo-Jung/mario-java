4b7bd130fc932d4a5780bfef02fc329bcee9b30a
tcfritchman <tcfritchman@gmail.com>
2021-06-24 22:01:02 -0700

    Draw a grid

--- src/editor/LevelEditor.java
+++ src/editor/LevelEditor.java
@@ -5,11 +5,50 @@ import java.awt.*;
 
 public class LevelEditor {
     private JPanel mainPanel;
-    private JScrollPane mapScrollPane;
+    private JScrollPane levelScrollPane;
     private JScrollPane toolScrollPane;
+    private JPanel levelPanel;
+
+    private int gridSize = 16;
+    private int levelWidth = 800;
+    private int levelHeight = 800;
 
     private void createUIComponents() {
-        // TODO: place custom component creation code here
+        levelPanel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                drawLevel(g);
+            }
+        };
+    }
+
+    private void drawLevel(Graphics g) {
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, levelWidth, levelHeight);
+        drawTiles(g);
+        drawGrid(g);
+    }
+
+    private void drawTiles(Graphics g) {
+
+    }
+
+    private void drawGrid(Graphics g) {
+        int width = levelWidth;
+        int height = levelHeight;
+
+        g.setColor(Color.CYAN);
+
+        // vertical lines
+        for (int i = gridSize; i < width; i += gridSize) {
+            g.drawLine(i, 0, i, height);
+        }
+
+        // horizontal lines
+        for (int i = gridSize; i < height; i+= gridSize) {
+            g.drawLine(0, i, width, i);
+        }
     }
 
     public static void main(String[] args) {
